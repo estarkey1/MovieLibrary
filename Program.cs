d330794ff122c3e6ad46fae0c846cf13f35daafd
estarkey1 <estarkey@my.wctc.edu>
2024-02-15 14:01:04 -0600

    NLog

--- Program.cs
+++ Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿using NLog;
+string path = Directory.GetCurrentDirectory() + "\\nlog.config";
+// create instance of Logger
+var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
+
